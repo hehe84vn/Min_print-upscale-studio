7ea11a2b45e0cba3bed649e5089d8e1640f55975
hehe84vn <ngoctunguyenle@gmail.com>
2026-07-26 12:37:17 +0700

    Wire Krea beta module behind licensed processing

--- src/main/bootstrap.js
+++ src/main/bootstrap.js
@@ -10,7 +10,8 @@ const PROTECTED_CHANNELS = new Set([
   'production:start',
   'benchmark:run',
   'model-studio:preview',
-  'color:convert'
+  'color:convert',
+  'krea-beta:enhance'
 ]);
 
 const originalHandle = ipcMain.handle.bind(ipcMain);
@@ -104,4 +105,5 @@ app.whenReady().then(async () => {
 
 require('./modelStudioV15Ipc');
 require('./updateManagerIpc');
+require('./kreaBetaIpc');
 require('./main');
